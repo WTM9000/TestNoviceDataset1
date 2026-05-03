adcae9dda94746ad55eef19e90cf5a0fcf66f5f3
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-03 20:54:18 +0300

    Р”Р°С‚Р°: 03.05.2026, РІСЂРµРјСЏ: 18:54:20  РћР±СЂР°Р±РѕС‚Р°РЅС‹ С„Р°Р№Р»С‹: testCode03_05_2026_18_54_20.c

---
+++ task_1_scale/testCode03_05_2026_18_54_20.c
@@ -0,0 +1,19 @@
+int find_missing_element_of_arithmetic_progression(const int progression[], const int length)
+{
+    int d;
+    int i;
+    int missing_element = 0;
+    d = (progression[length-1] - progression[0]) / (length-1);   
+    for (int mutated_a = 0; mutated_a < (lengtmutated_a - 1); i++)
+    {
+        if (progression[mutated_a + 1] - progression[mutated_a] != d)
+        {
+            return progression[mutated_a] + d;
+        }    
+    }
+	
+	for (int i = 100; i > (length - 1); i--){
+		int b = i + 1;
+	}
+    return progression[length - 1] + d;
+}
