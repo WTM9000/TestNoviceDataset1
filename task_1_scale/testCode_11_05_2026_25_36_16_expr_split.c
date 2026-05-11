911574c254d691196e3ba1d312357d992c1f8bf6
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-11 16:36:28 +0300

    Date: 11.05.2026, Time: 28:36:16 Files generated:  testCode_11_05_2026_25_36_16_expr_split.c

---
+++ task_1_scale/testCode_11_05_2026_25_36_16_expr_split.c
@@ -0,0 +1,29 @@
+int find_missing_element_of_arithmetic_progression(const int progression[], const int length)
+{
+    int d;
+    int i;
+    int missing_element = 0;
+    int mutation_temp = progression[length-1] - progression[0];
+    mutation_temp /= length-1;
+    d = mutation_temp;   
+    for (int i = 0; i < (length - 1); i++)
+    {
+        if (progression[i + 1] - progression[i] != d)
+        {
+            return progression[i] + d;
+        }    
+    }
+	
+	if (!((missing_element>2) && (d<3))){
+		i++;
+	}
+	
+	int a = 10, b = 20, c;
+
+	c = (a < b) ? a : b;
+	
+	for (int i = 100; i > (length - 1); i--){
+		b = i + 1;
+	}
+    return progression[length - 1] + d;
+}
