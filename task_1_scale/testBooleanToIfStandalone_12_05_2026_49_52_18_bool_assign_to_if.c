2b5f28c2c5572b5125463c0e372fa19af577e020
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-12 18:52:52 +0300

    Date: 12.05.2026, Time: 52:52:18 Files generated:  testBooleanToIfStandalone_12_05_2026_49_52_18_bool_assign_to_if.c

---
+++ task_1_scale/testBooleanToIfStandalone_12_05_2026_49_52_18_bool_assign_to_if.c
@@ -0,0 +1,44 @@
+int find_missing_element_of_arithmetic_progression(const int progression[], const int length)
+{
+    int d;
+    int i;
+    int missing_element = 0;
+    d = (progression[length-1] - progression[0]) / (length-1);   
+    for (int i = 0; i < (length - 1); i++)
+    {
+        if (progression[i + 1] - progression[i] != d)
+        {
+            return progression[i] + d;
+        }    
+    }
+	
+	if (!((missing_element>2) && (d<3))){
+		i++;
+	} else if (d >10){
+		d++;
+	} else if (d<2){
+		d--;
+	} else {
+		d+=10;
+	}
+	int f;
+	
+	while (f=(d<length)){
+		i++;
+		if (missing_element > 3) f = 1;
+		else f = 0;
+		return f;
+	}
+	
+	int a = 10, b = 20, c;
+
+	c = (a < b) ? a : b;
+	
+	if (a>b)
+		i++;
+	
+	for (int i = 100; i > (length - 1); i--){
+		b = i + 1;
+	}
+    return progression[length - 1] + d;
+}
