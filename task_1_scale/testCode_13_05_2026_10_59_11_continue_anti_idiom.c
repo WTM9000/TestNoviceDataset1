4143f2dc9474e653d9a7d91e0959f7b6b9102ee9
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-13 11:59:13 +0300

    Date: 13.05.2026, Time: 13:59:11 Files generated:  testCode_13_05_2026_10_59_11_continue_anti_idiom.c

---
+++ task_1_scale/testCode_13_05_2026_10_59_11_continue_anti_idiom.c
@@ -0,0 +1,40 @@
+int find_missing_element_of_arithmetic_progression(const int progression[], const int length)
+{
+    int d;
+    int i;
+    int missing_element = 0;
+    d = (progression[length-1] - progression[0]) / (length-1);   
+    for (int i = 0; i < (length - 1); i++)
+    {
+        if (!(progression[i + 1] - progression[i] != d))
+            continue;
+        return progression[i] + d;
+    }
+	
+	if (!((missing_element>2) && (d<3))){
+		i++;
+	} else if (d >10){
+		d++;
+	} else if (d<2){
+		d--;
+	} else {
+		d+=10;
+	}
+	int f = (d != length);
+	
+	f = (d > length) && (missing_element < 3);
+
+	while (f=(d<length)){
+		i++;
+		return f = missing_element > 3;
+	}
+	
+	int a = 10, b = 20, c;
+
+	c = (a < b) ? a : b;
+	
+	for (int i = 100; i > (length - 1); i--){
+		b = i + 1;
+	}
+    return progression[length - 1] + d;
+}
