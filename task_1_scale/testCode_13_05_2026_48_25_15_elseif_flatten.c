0a5c6bcb17431003ed8f9fd978d7d42aa186d80a
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-13 15:25:52 +0300

    Date: 13.05.2026, Time: 51:25:15 Files generated:  testCode_13_05_2026_48_25_15_elseif_flatten.c

--- task_1_scale/testCode_13_05_2026_48_25_15_elseif_flatten.c
+++ task_1_scale/testCode_13_05_2026_48_25_15_elseif_flatten.c
@@ -12,14 +12,18 @@ int find_missing_element_of_arithmetic_progression(const int progression[], cons
         }    
     }
 	
-	if (!((missing_element>2) && (d<3))){
-		i++;
-	} else if (d >10){
-		d++;
-	} else if (d<2){
-		d--;
+	if (!((missing_element>2) && (d<3))) {
+	    i++;
 	} else {
-		d+=10;
+	    if (d >10) {
+	        d++;
+	    } else {
+	        if (d<2) {
+	            d--;
+	        } else {
+	            d+=10;
+	        }
+	    }
 	}
 	
 	int f = (d != length);
